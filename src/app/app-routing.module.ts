e42bb34e42ea85122172d8bbc48b9297395433b6
Murillo Nahás <murillonahas@hotmail.com>
2022-03-16 16:16:05 -0300

    feat: setup blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TagsComponent } from './pages/tags/tags.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'blog', component: BlogComponent
+  },
+  {
+    path: 'tags', component: TagsComponent
+  },
+  {
+    path: 'projects', component: ProjectsComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: '**', redirectTo: 'error-page'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { TagsComponent } from './pages/tags/tags.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BlogComponent,
+    TagsComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
